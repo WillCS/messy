8e749298e669fcafb974786872d594d245603746
WillCS <wcstibbards@gmail.com>
2017-09-19 17:44:08 +1000

    Rudimentary work on reading messages and displaying sender.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var fs = require("fs");
-var prompt = require('prompt');
-var login = require("facebook-chat-api");
+const fs = require("fs");
+const prompt = require('prompt');
+const login = require("facebook-chat-api");
+const readline = require("readline");
 
-if(fs.existsSync('appstate.json')) {
+if(fs.existsSync("appstate.json")) {
     main({
         appState: JSON.parse(fs.readFileSync("appstate.json", "utf8"))
     });
@@ -29,18 +30,8 @@ if(fs.existsSync('appstate.json')) {
             email: result.email,
             password: result.password
         });
-    });
-    
+    });   
 }
-
-var authInput = {
-    properties: {
-        code: {
-            required: true,
-            message: "Enter your two-factor authentication code"
-        }
-    }
-};
 
 function main(credentials) {
     var options = {
@@ -62,12 +53,59 @@ function main(credentials) {
             }
         }
 
+        var stream = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            terminal: false
+        });
+
+        stream.on("line", (line) => {
+            if(line === "logout") {
+                api.logout((err) => {
+                    if(!err) {
+                        fs.unlinkSync("appstate.json");
+                        console.log("Logged out");
+                        process.exit();
+                    } else {
+                        console.log("There was a problem logging out");
+                    }
+                });
+            }
+
+            if(line === "exit") {
+                exit();
+            }
+        });
+
+        function exit() {
+            stream.question("Are you sure you want to exit? ", (answer) => {
+                if(answer === "yes") {
+                    process.exit();
+                }
+            });
+        }
+
+        stream.on("SIGINT", exit);
+        stream.on("SIGTSTP", exit);
+
         console.log("Logged in");
 
-        fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+        fs.writeFileSync("appstate.json", JSON.stringify(api.getAppState()));
 
-        api.listen((err, message) => {
-            console.error(message.body);
+        api.listen((err, event) => {
+            switch(event.type) {
+                case "message":
+                    api.getUserInfo(event.senderID, (err, obj) => {
+                        if(err) {
+
+                        } else {
+                            console.log(obj[event.senderID].name + ": " + event.body);
+                        }
+                    });
+                    break;
+            }
+            
+           // console.log(event.body);
             //api.sendMessage(message.body, message.threadID);
         });
     });
